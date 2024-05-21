8964db081d401dc5deeff79e83db057ecd79efac
Pilotebas <149727792+Pilotebas@users.noreply.github.com>
2024-05-21 09:20:32 -0500

    fix: :bug: Se corrigen las funciones

--- js/modules/modules.js
+++ js/modules/modules.js
@@ -1,20 +1,17 @@
 export const getAllabrigos = async() =>{
     let rest = await fetch ('http://localhost:5501/abrigo')
     let data = await rest.json();
-    console.log(data);
-    return dataUpdate;
+    return data;
 }
 
 export const getAllcamiseta = async() =>{
     let rest = await fetch ('http://localhost:5501/camiseta')
     let data = await rest.json();
-    console.log(data);
-    return dataUpdate;
+    return data;
 }
 
 export const getAllpantalon = async() =>{
     let rest = await fetch ('http://localhost:5501/pantalon')
     let data = await rest.json();
-    console.log(data);
-    return dataUpdate;
+    return data;
 }
